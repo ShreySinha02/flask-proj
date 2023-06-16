42329079ec3500e6671ab2ff202fb8faea486f29
ShreySinha01 <shrey8340@gmail.com>
2023-06-17 00:38:28 +0530

    added jobitem

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
 
-
+JOBS=[
+    {
+        'id':1,
+        'title':"data analyst",
+        'location':"bengaluru"
+    },
+    {
+        'id':2,
+        'title':"web developer",
+        'location':"bengaluru"
+    },
+    {
+        'id':3,
+        'title':"ui/ux",
+        'location':"bengaluru"
+    }
+    
+]
 app=Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello"
+    return render_template('home.html',jobs=JOBS)
 
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
